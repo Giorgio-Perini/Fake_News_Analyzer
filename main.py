494920d74c16b8767e1a7eaac17208df65e37b2e
Giorgio-Perini <perini.giorgio@iisgalvanimi.edu.it>
2025-05-08 09:28:56 +0000

    no comment

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
 from flask import Flask, render_template, request
 import pandas as pd
 
-app = Flask(__name__, static_folder='templates')
+app = Flask(__name__)
 
 
 def carica_parole_csv(file_path):
     df = pd.read_csv(file_path)
-    parole = dict(zip(df['parola'].str.lower(), df['punteggio']))  
+    parole = dict(zip(df['parola'].str.lower(), df['punteggio'])) 
     return parole
 
 def calcola_punteggio(titolo, parole):
     punteggio_totale = 0
-    parole_titolo = titolo.lower().split() #divide e minuscolo
+    parole_titolo = titolo.lower().split()
 
     for parola in parole_titolo:
         if parola in parole:
             punteggio_totale += parole[parola]
 
     return punteggio_totale
-
-def calcola_punteggio_percentuale(titolo, parole):
-    punteggio_totale = 0
-    parole_titolo = titolo.lower().split()
-    totale_parole = len(parole_titolo)
-
-    for parola in parole_titolo:
-        if parola in parole:
-            punteggio_totale += parole[parola]
-
-    if totale_parole > 0:
-        return (punteggio_totale / totale_parole) * 100
-    return 0
 
 def classificazione(punteggio):
     if punteggio > 0:
@@ -45,9 +32,10 @@ def index():
     if request.method == 'POST':
         titolo = request.form['titolo']  
         parole = carica_parole_csv("parole.csv")  
-        punteggio_percentuale = calcola_punteggio_percentuale(titolo, parole)
-        classe = classificazione(punteggio_percentuale)
-        return render_template('index.html', titolo=titolo, punteggio=round(punteggio_percentuale, 2), classe=classe)
+        punteggio = calcola_punteggio(titolo, parole)
+        classe = classificazione(punteggio)
+        return render_template('index.html', titolo=titolo, punteggio=round(punteggio, 2), classe=classe)
+    
     return render_template('index.html', titolo=None, punteggio=None, classe=None)
 
 if __name__ == '__main__':
